3a4745524dc8395acbcb0f4d8800a3ac63c23508
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-04 23:02:12 +0900

    retest1

--- test.bash
+++ test.bash
@@ -1,22 +1,4 @@
 #!/bin/bash
 
-tmp=/tmp/$$
-
-ERROR_EXIT () {
-	echo "$1" >&2
-	rm -f $tmp-*
-	exit 1
-}
-
-###########################
-# TEST1
-###########################
-
-echo 100 NG > $tmp-ans
-./mintest.bash 100 10 > $tmp-out || ERROR_EXIT "TEST1-1"
-diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
-
-echo OK
-rm -f $tmp-*
-exit 0
-
+echo "This is a Test."
+echo "AAAAA"
