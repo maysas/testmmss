6d69d8a0827046496617970d0f772c153aecf179
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-05 00:33:30 +0900

    retest11

--- mintest.bash
+++ mintest.bash
@@ -6,22 +6,22 @@ min=$2;
 memo=0;
 
 if [ $max -lt $min ] ; then
-	memo=$max;
-	max=$min;
-	min=$memo;
+    memo=$max;
+    max=$min;
+    min=$memo;
 fi
 
 if [ $min -ne 0 ] ; then
-	a=`expr $max % $min` ;
+    a=`expr $max % $min` ;
 else
-	a=0
+    a=0
 fi
 
 while [ $a -ne 0 ]
 do
-	max=$min;
-	min=$a;
-	a=`expr $max % $min`
+    max=$min;
+    min=$a;
+    a=`expr $max % $min`
 done;
 
 echo $min
